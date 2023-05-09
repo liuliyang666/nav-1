9f5eb12b6348e140e9adcc81482202ae8422f8ca
lilyyang <1592218619@qq.com>
2023-05-09 18:59:54 +0800

    修改关闭打开链接的错误

--- src/main.js
+++ src/main.js
@@ -1,70 +1,73 @@
-const $siteList = $('.siteList')
-const $lastLi = $siteList.find('li.last')
-const x = localStorage.getItem('x')
-const xObject = JSON.parse(x)
-const hasMap = xObject || [
-    { logo: 'G', url: 'https://github.com'},
-    { logo: 'J', url: 'https://juejin.cn'},
-    { logo: 'F', url: 'https://www.figma.com'},
-    { logo: 'M', url:'https://developer.mozilla.org'},
-    { logo: 'X', url:'https://xiedaimala.com'},
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+//读取当前网站下的x，如果x能成功的变成一个对象，那就把这个对象放在hashMap里；如果不能，我就把它初始化成有两项的数组
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: "A", url: "https://www.acfun.cn" },
+  { logo: "B", url: "https://m.bilibili.com" },
 ];
-const simplifyUrl = (url) =>{ 
-    return url.replace('https://', '')
-    .replace('http://', '')
-    .replace('www.', '')
-    .replace(/\/.*/, '') //删除/开头的内容
-}
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); //删除 /
+};
 
-const render = ()=>{
-    $siteList.find('li:not(.last)').remove()
-    hasMap.forEach((node, index) => {
-        const $li = $(`<li>
-          <div class="site">
-            <div class="logo">${node.logo}
-            </div>
-            <div class="link">${simplifyUrl(node.url)}</div>
-            <div class="close">
-             <svg class="icon">
-              <use xlink:href="#icon-close"></use>
-             </svg>
-            </div>
-          </div>
-      </li>`).insertBefore($lastLi)
-      $li.on('click', () =>{
-          window.open(node.url)
-      })
-      $li.on('click', '.close', (e) =>{
-          e.stopPropagation //阻止冒泡
-          hasMap.splice(index, 1)
-          render()
-      })
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  //找到除了 .last 的 li ，并删除，也就是删掉之前本有的那些li，接着forEach遍历hashMap重新添加旧的li和新的li
+  hashMap.forEach((node, index) => {
+    const $li = $(`<li>
+            <div class="site">
+                <div class="logo">${node.logo}</div >
+                <div class="link">${simplifyUrl(node.url)}</div>
+                <div class="close">
+                <svg class="icon">
+                <use xlink:href="#icon-close"></use>
+               </svg>
+                </div>
+            </div >
+        </li > `).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
     });
-}
-render()
-$('.addButton').on('click', ()=>{
-      let url = window.prompt('请输入你要添加的网址。')
-      if(url.indexOf('http')!==0){
-         url = 'https://' + url
-      }
-      console.log(url)
-      hasMap.push({
-          logo: simplifyUrl(url)[0].toUpperCase(),
-          url: url
-      });
-      render()
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); //阻止冒泡
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问你要添加的网址是啥？");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  console.log(url);
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url,
+  });
+  render();
 });
 
-window.onbeforeunload = ()=>{
-   const string = JSON.stringify(hasMap)
-   localStorage.setItem('x', string)
-}
-$(document).on('keyprass', (e) =>{
-    const {key} = e
-    for(let i = 0; i<hasMap.length; i++) {
-        if(hasMap[i].logo.toLowerCase() === key){
-            window.open(hasMap[i].url)
-        }
-    }
-})
-  
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
+
+// 用户体验不好，搜索时候会打开网站
+// $(document).on('keypress', (e) => {
+//     const { key } = e
+//     console.log()
+//     for (let i = 0; i < hashMap.length; i++) {
+//         // if (hashMap[i].logo.toLowerCase() === key) {
+//         //     window.open(hashMap[i].url)
+//         // }
+//     }
+// })
